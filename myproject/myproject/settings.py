2d666fa5e5c230aef536d639fb03a9d1f532851e
wakeel <your-email@example.com>
2025-08-05 16:30:58 +0530

    setting update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mudra_db',
-        'USER': 'mudra_user',
-        'PASSWORD': 'StrongPassword123',
+        'NAME': 'kreditbee',
+        'USER': 'root',
+        'PASSWORD': 'Wakeel@321',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -117,9 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Default primary key field type
